2f3c6102003f945c77d999b7e088bea2b22f6d34
JPedroBorges <JPedroBorges@users.noreply.github.com>
2025-06-20 03:47:02 +0100

    add self ip retrieval

---
+++ ElephLink/src/main/java/com/roomelephant/elephlink/adapters/ipservice/IpService.java
@@ -0,0 +1,58 @@
+package com.roomelephant.elephlink.adapters.ipservice;
+
+import com.roomelephant.elephlink.domain.model.IpServiceConfig;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class IpService {
+  private static final Pattern ipv4Pattern = Pattern.compile("^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$");
+  private static final HttpClient client = HttpClient.newHttpClient();
+
+  private final List<HttpRequest> requests;
+
+  public IpService(IpServiceConfig config) {
+    try {
+      this.requests = config.getIps().stream().map(ipService -> HttpRequest.newBuilder()
+          .uri(URI.create(ipService))
+          .GET()
+          .build()).toList();
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Invalid IP address - " + e.getMessage());
+    }
+  }
+
+  public Optional<String> fetchCurrentIp() {
+    for (HttpRequest request : requests) {
+      HttpResponse<String> response = request(request);
+
+      if (response == null) {
+        continue;
+      }
+
+      String ip = response.body().trim();
+      if (ipv4Pattern.matcher(ip).matches()) {
+        log.info("Current public IP {}", ip);
+        return Optional.of(ip);
+      }
+    }
+
+    return Optional.empty();
+  }
+
+  private HttpResponse<String> request(HttpRequest request) {
+    try {
+      return client.send(request, HttpResponse.BodyHandlers.ofString());
+    } catch (Exception e) {
+      log.debug("Failed to fetch current ip", e);
+      Thread.currentThread().interrupt();
+      return null;
+    }
+  }
+}
